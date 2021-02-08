5bdc9bf1e4b2553fe633ddf931cb37351074620b
tomeriffic <tpilvelis@ymail.com>
2021-02-08 14:11:28 +0000

    WIP

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from typing import Optional
 from fastapi import FastAPI
+from jira import JIRA
+import yaml
+
 from fastapi.middleware.cors import CORSMiddleware
 
 from azure.devops.connection import Connection
@@ -24,7 +27,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 def hello_world():
     return {"Hello": "World"}
@@ -45,11 +47,18 @@ def get_user_story_children_ids(work_item):
     return child_ids
 
 def get_azure_devops_connection():
-    personal_access_token = '<personal_access_token>'
-    organization_url = '<organization_url>'
+    personal_access_token = 'and6ey5t35ucjfgpneeollyyezfn7bgxxenqi6sjzzqcxbnqvcrq'
+    organization_url = 'https://dev.azure.com/glasswall'
     credentials = BasicAuthentication('', personal_access_token)
     connection = Connection(base_url=organization_url, creds=credentials)
     return connection
+
+def get_jira_connection():
+    options = {"server": "https://glasswall.atlassian.net"}
+    API_KEY = "iCegsRUWSh8xbOxO5qoG00C1"
+    user = "tpilvelis@glasswallsolutions.com"
+    jira = JIRA(options, basic_auth=(user, API_KEY))
+    return jira
 
 def get_work_item_title(work_item):
     return work_item.fields['System.Title']
@@ -79,3 +88,40 @@ def read_item(user_story_id: int):
         "user_story_name": work_item_name,
         "tasks" : tasks
     }
+
+@app.get("/sync")
+def sync_devops_to_jira():
+    jira_conn = get_jira_connection()
+    ado_conn = get_azure_devops_connection()
+
+    data = None
+    with open("config.yml", 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            return {
+                "statusCode" : 400,
+                "body" : str(exc)
+            }
+    print(data)
+
+    data_to_sync = data['ToSync']
+
+    for program in data_to_sync['PROGRAM']:
+        print(program['Name'])
+        issues = jira_conn.search_issues(f"project = PROGRAM AND text ~ \"{program['Name']}\"")
+        if len(issues):
+            print("PROGRAM Found...")
+
+        for project in program['PROJECT']:
+            print(project['Name']) 
+
+            for feature in project['FEATURE']:
+                print(feature['Name'])
+
+                for story in feature['STORY']:
+                    print(str(story))
+
+
+
+    print(".")
